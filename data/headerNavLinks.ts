1b447ee9a15dd2bf1113c33b153f59b5c83e1653
Chad <chad97@126.com>
2026-06-25 18:19:31 +0800

    docs: clean migrated blog template traces

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/about', title: 'About' },
+  { href: '/', title: '首页' },
+  { href: '/blog', title: '文章' },
+  { href: '/tags', title: '标签' },
+  { href: '/about', title: '关于' },
 ]
 
 export default headerNavLinks
